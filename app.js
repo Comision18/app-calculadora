9fe492e3ca738bf7b5f3e7cdd70a1a438be54177
Eric <menaericdaniel@gmail.com>
2022-11-03 10:57:33 -0300

    estructura inicial

---
+++ app.js
@@ -0,0 +1,13 @@
+const calculadora = require('./modulos/calculadora')
+switch (comando) {
+    case "sumar":
+        
+        break;
+    case "restar":
+    
+        break;
+    
+
+    default:
+        break;
+}
