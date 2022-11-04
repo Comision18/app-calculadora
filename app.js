e2e541437cc2fabb2e59ea5f0992756cdd883364
Eric <menaericdaniel@gmail.com>
2022-11-04 09:31:50 -0300

    agrega funcionalidad restar

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const {sumar} = require('./modulos/calculadora')
+const {sumar, restar} = require('./modulos/calculadora')
 switch (comando) {
     case "sumar":
         console.log(sumar(2,3))
         break;
     case "restar":
-    
+        console.log(restar(2,3))
+
         break;
     
 
